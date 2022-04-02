30acfb37acfd00ca53459c2d10f752c96beda4f5
gchaotic9 <ronidaou95@gmail.com>
2022-04-02 02:18:53 -0400

    Create resizeLogin.js
    Resizes window

---
+++ src/resizeLogin.js
@@ -0,0 +1,8 @@
+const gui = require("nw.gui");
+let win = gui.Window.get();
+win.setResizable(true);
+win.unmaximize();
+win.leaveFullscreen();
+win.resizeTo(392, 518);
+win.setResizable(false);
+// win.resizeTo(392, 518);
