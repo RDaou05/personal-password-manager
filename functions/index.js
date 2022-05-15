981b3a089d841b0cacd4d28c8d2d5f6cd8ecc5cd
gchaotic9 <ronidaou95@gmail.com>
2022-05-15 00:17:25 -0400

    Make SecKey and UpateRawData Functions
    Made a function to add a secret key for each user when they sign up. Also finished a function to update user queries from the backend.

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,76 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const CryptoJS = require("crypto-js");
 admin.initializeApp();
 
-exports.queryCounter = functions.firestore
-  .document("/users/filler/{userId}/mpaps/ps/{passQuery}")
-  .onCreate((snap, context) => {
-    const db = admin.firestore();
-    const userUID = context.params.uid;
-    db.doc("/tempcol/tempcoldoc")
-      .get()
-      .then((gotten) => {
-        db.doc(`/users/filler/${userUID}/mpaps/limits/numofq`)
-          .set({
-            qamount: gotten,
-            ofwritten: userUID,
-          })
-          .then(() => {
-            return {
-              message: "Yep it works!",
-            };
-          })
-          .catch((err) => {
-            return {
-              error: `Error is: ${err}`,
-            };
-          });
-      });
+exports.updateRawData = functions.https.onCall((data, context) => {
+  const nummy = admin.firestore.FieldValue.serverTimestamp();
+  const rawObjectToUpdate = data.objectToUpdate;
+  const randomID = data.randomID;
+  const isLink = data.isLink;
+  let encryptedObjectToUpdate = {};
+  const encryptionKey = data.hashedSetMasterPassValue;
+  const db = admin.firestore();
+  const updatePath = db
+    .collection("users")
+    .doc("filler")
+    .collection(data.userUID)
+    .doc("mpaps")
+    .collection("ps")
+    .doc(data.sourceRefID);
+
+  let infoArray = [];
+
+  for (const [key, value] of Object.entries(rawObjectToUpdate)) {
+    infoArray.push(`${key}: ${value}`);
+    encryptedObjectToUpdate[key] = CryptoJS.AES.encrypt(
+      value,
+      encryptionKey
+    ).toString();
+  }
+
+  encryptedObjectToUpdate.isLink = isLink;
+  encryptedObjectToUpdate.random = randomID;
+
+  const batch = db.batch();
+  const stringifiedEncryptedObject = JSON.stringify(encryptedObjectToUpdate);
+  batch.update(updatePath, {
+    combinedQueryInfo: stringifiedEncryptedObject,
+    nummy: nummy,
   });
+  batch
+    .commit()
+    .then(() => {
+      return {
+        data: data,
+        encryptedObj: encryptedObjectToUpdate,
+        strEncryptedObj: stringifiedEncryptedObject,
+        updatePath: updatePath,
+        infoArray: infoArray,
+        rawObjectToUpdate: rawObjectToUpdate,
+      };
+    })
+    .catch((err) => {
+      return {
+        ERROR: err,
+        data: data,
+        encryptedObj: encryptedObjectToUpdate,
+        strEncryptedObj: stringifiedEncryptedObject,
+        updatePath: updatePath,
+        infoArray: infoArray,
+        rawObjectToUpdate: rawObjectToUpdate,
+      };
+    });
+
+  return {
+    data: data,
+    encryptedObj: encryptedObjectToUpdate,
+    strEncryptedObj: stringifiedEncryptedObject,
+    updatePath: updatePath,
+    infoArray: infoArray,
+    rawObjectToUpdate: rawObjectToUpdate,
+  };
+});
 
 exports.givePRole = functions.https.onCall((data, context) => {
   const usersUID = data.uid;
@@ -56,6 +100,29 @@ exports.givePRole = functions.https.onCall((data, context) => {
       return err;
     });
   return Promise.all([p1, p2]);
+});
+
+exports.giveSignUpSecret = functions.auth.user().onCreate((user) => {
+  function makeid(length) {
+    var result = "";
+    var characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result; // Returns a VERY long unique random id
+  }
+  const db = admin.firestore();
+  const randomSecretStr = makeid(100); /* The secret random string that is added
+  to the end of user encryptions. Right now, we are creating and saving that string to be used later*/
+  const p1 = db
+    .collection("users")
+    .doc("filler")
+    .collection(usersUID)
+    .doc("secKey")
+    .set({ secKeyStr: randomSecretStr });
+  return Promise.all([p1]);
 });
 
 exports.giveSignUpRoles = functions.auth.user().onCreate((user) => {
