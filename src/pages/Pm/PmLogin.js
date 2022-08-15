44f01814645aa89e9f6b3739322c73880a8592c7
gchaotic9 <ronidaou95@gmail.com>
2022-08-14 22:58:42 -0400

    Added master password check process

--- src/pages/Pm/PmLogin.js
+++ src/pages/Pm/PmLogin.js
@@ -1,7 +1,47 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
+import { checkifMasterPasswordIsCorrect } from "../../firebase";
 
 const PmLogin = () => {
-  return <div>LOGIn</div>;
+  const [loginInputState, setLoginInputState] = useState("");
+  const [masterPasswordIsCorrectState, setMasterPasswordIsCorrectState] =
+    useState(false);
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(event) => {
+          setLoginInputState(event.target.value);
+        }}
+        onKeyUp={async (event) => {
+          if (event.key == "Enter") {
+            const masterPasswordIsCorrect =
+              await checkifMasterPasswordIsCorrect(event.target.value);
+            if (masterPasswordIsCorrect) {
+              setMasterPasswordIsCorrectState(true);
+            } else {
+              setMasterPasswordIsCorrectState(false);
+            }
+          }
+        }}
+      />
+      <button
+        onClick={async () => {
+          const masterPasswordIsCorrect = await checkifMasterPasswordIsCorrect(
+            loginInputState
+          );
+          if (masterPasswordIsCorrect) {
+            setMasterPasswordIsCorrectState(true);
+          } else {
+            setMasterPasswordIsCorrectState(false);
+          }
+        }}
+      ></button>
+      {masterPasswordIsCorrectState ? <div>YESSS</div> : <div>NOOOO</div>}
+      {/* Do do when get back from beach:
+        just finished master pass sign up and check login process. Next work on Displaying screen when master pass is correct and when wrong. (Make UI for login page)
+      */}
+    </div>
+  );
 };
 
 export default PmLogin;
