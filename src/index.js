d1aae9c1a7111c9b43d90b45f52a63dba3440dfd
gchaotic9 <83796134+gchaotic9@users.noreply.github.com>
2022-11-24 21:47:43 -0500

    New template fixed!

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React, { StrictMode } from "react";
 import "./index.css";
 import App from "./App.js";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
