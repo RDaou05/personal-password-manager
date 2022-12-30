8ac91abcabd97d31fb9c9bd52ed4c8877423c3c8
gchaotic9 <83796134+gchaotic9@users.noreply.github.com>
2022-12-30 15:02:28 -0500

    Current bug: have to log out for email ver to update

--- src/pages/AppSelector/PleaseVerify.js
+++ src/pages/AppSelector/PleaseVerify.js
@@ -6,36 +6,44 @@ import { reload } from "firebase/auth";
 
 const PleaseVerify = (props) => {
   return (
-    <div>
-      <p id={classes.message}>
-        Please verify your email at{" "}
-        <span id={classes.email}>{firebaseAuth.currentUser.email}</span>
-      </p>
-      <button
-        id={classes.sendLinkButton}
-        onClick={async () => {
-          await resendLink();
-        }}
-      >
-        Resend Verification Link
-      </button>
-      <button
-        id={classes.refreshButton}
-        onClick={async () => {
-          // Getting the updated version of the auth token
-          await reload(firebaseAuth.currentUser);
-          if (firebaseAuth.currentUser.emailVerified) {
-            props.setEmailVerifiedState(true);
-          } else {
-            props.setEmailVerifiedState(false);
-          }
-        }}
-      >
-        Retry
-      </button>
-      <Link to="/" id={classes.backToLogin}>
-        Back To Login
-      </Link>
+    <div className={classes.pleaseVerifyScreen}>
+      <div className={classes.pleaseVerifyPopup}>
+        <p id={classes.message}>
+          Please verify your email at{" "}
+          <span id={classes.email}>{firebaseAuth.currentUser.email}</span>
+        </p>
+        <div className={classes.buttonsContainer}>
+          <button
+            id={classes.sendLinkButton}
+            className={classes.controlButtons}
+            onClick={async () => {
+              await resendLink();
+            }}
+          >
+            Resend Verification Link
+          </button>
+          <button
+            id={classes.refreshButton}
+            className={classes.controlButtons}
+            onClick={async () => {
+              // Getting the updated version of the auth token
+              await reload(firebaseAuth.currentUser);
+              if (firebaseAuth.currentUser.emailVerified) {
+                props.setEmailVerifiedState(true);
+              } else {
+                props.setEmailVerifiedState(false);
+              }
+            }}
+          >
+            Retry
+          </button>
+        </div>
+        <div className={classes.backToLoginContainer}>
+          <Link to="/" id={classes.backToLogin}>
+            Back To Login
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
