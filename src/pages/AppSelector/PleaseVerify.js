05d1624bed64c0a6f4da97f58f382a3eea085b6d
gchaotic9 <ronidaou95@gmail.com>
2023-06-16 02:26:33 -0400

    Loading

--- src/pages/AppSelector/PleaseVerify.js
+++ src/pages/AppSelector/PleaseVerify.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { firebaseAuth, resendLink } from "../../firebase";
 import classes from "./PleaseVerify.module.css";
 import { Link } from "react-router-dom";
 import { getIdToken, reload, updateCurrentUser } from "firebase/auth";
+import LoadingScreen from "../../components/LoadingScreen";
 
 const PleaseVerify = (props) => {
+  const [loadingScreenState, setLoadingScreenState] = useState(false);
   return (
     <div className={classes.pleaseVerifyScreen}>
-      <div className={classes.pleaseVerifyPopup}>
+      {loadingScreenState ? <LoadingScreen /> : null}
+      <div
+        className={classes.pleaseVerifyPopup}
+        style={{ opacity: loadingScreenState ? "0.5" : "1" }}
+      >
         <p id={classes.message}>
           Please verify your email at{" "}
           <span id={classes.email}>{firebaseAuth.currentUser.email}</span>
@@ -28,13 +34,16 @@ const PleaseVerify = (props) => {
             className={classes.controlButtons}
             style={{ cursor: "pointer" }}
             onClick={async () => {
+              setLoadingScreenState(true);
               // Getting the updated version of the auth token
               await reload(firebaseAuth.currentUser);
               await updateCurrentUser(firebaseAuth, firebaseAuth.currentUser);
               firebaseAuth.currentUser.getIdToken(true);
               if (firebaseAuth.currentUser.emailVerified) {
+                setLoadingScreenState(false);
                 props.setEmailVerifiedState(true);
               } else {
+                setLoadingScreenState(false);
                 props.setEmailVerifiedState(false);
               }
             }}
